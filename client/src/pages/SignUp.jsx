839e0afb11c0dc18b972f2eb0b7799dc282a0156
Omkar Jadhav <jadhavomkar1408@gmail.com>
2024-02-29 20:55:05 +0530

    complete sign-up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,38 +1,106 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-  return (
-    <>
+    const [formData, setFormData] = useState({})
+    const [error , SetError] = useState(null);
+const [loading  ,SetLoading]=useState(false)
 
-    <div className='p-3 max-w-lg mx-auto'>
-    <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+const navigate = useNavigate();
+    
 
-    <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='username'
-        className='border p-3 rounded-lg ' id='username'/>
 
-          <input type='email' placeholder='email'
-           className='border p-3 rounded-lg ' id='email'/>
 
-        <input type='password' placeholder='password'
-        className='border p-3 rounded-lg ' id='password'/>
 
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'> Sign up </button>
+    const handleChange = (e) => {
+        setFormData(
+            {
+                ...formData,
+                [e.target.id]: e.target.value,
+            }
+        );
+    };
 
-    </form>
 
-    <div className='flex gap-2 mt-5'>
-      
-        <p> have an account?</p>
-        <Link to={"/sign-in"}>
-            <span className='text-blue-700 '> Sign in</span>
-        </Link>
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-    </div>
-    </div>
-    </>
-  )
+    try{
+
+        SetLoading(true)
+        const res = await fetch('/api/auth/signup',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+    
+            }
+    
+        );
+    
+        const data = await res.json();
+    
+    
+        if(data.success === false){
+            SetLoading(false);
+            SetError(data.message);
+           
+            return;
+        }
+        SetLoading(false)
+        SetError(null)
+        navigate('sign-in');
+        console.log(data)
+    }
+     catch(error) {
+      SetLoading(false);
+      SetError(error.message)
+
+    }
+
+
+    };
+
+    console.log(formData);
+
+
+
+
+    return (
+        <>
+
+            <div className='p-3 max-w-lg mx-auto'>
+                <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+
+                <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+                    <input type='text' placeholder='username'
+                        className='border p-3 rounded-lg ' id='username' onChange={handleChange} />
+
+                    <input type='email' placeholder='email'
+                        className='border p-3 rounded-lg ' id='email' onChange={handleChange} />
+
+                    <input type='password' placeholder='password'
+                        className='border p-3 rounded-lg ' id='password' onChange={handleChange} />
+
+                    <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'> {loading ? 'Loading...' : 'Sign Up'} </button>
+
+                </form>
+
+                <div className='flex gap-2 mt-5'>
+
+                    <p> have an account?</p>
+                    <Link to={"/sign-in"}>
+                        <span className='text-blue-700 '> Sign in</span>
+                    </Link>
+ 
+                </div>
+
+                {error && <p className='text-red-500 mt-5'>{error}</p>}
+            </div>
+        </>
+    )
 }
 
 export default SignUp
